4633c2e16cdad3ece320f23192b229890992a063
wadldoo <wadldoo@yahoo.co.jp>
2016-05-02 16:36:54 +0900

    [Modify] adopt to es2015

--- main.js
+++ main.js
@@ -1,35 +1,32 @@
 'use strict';
 
-var app = require('app');
-var BrowserWindow = require('browser-window');
-var mainWindow = null;
+const app = require('app'),
+      BrowserWindow = require('browser-window');
 
-app.on('window-all-closed', function() {
-    if (process.platform != 'darwin') {
-        app.quit();
-    }
+let mainWindow = null;
+
+app.on('window-all-closed', () => {
+  if (process.platform != 'darwin') app.quit();
 });
 
-app.on('ready', function() {
-    var Screen = require('screen');
-    var size = Screen.getPrimaryDisplay().size;
-    mainWindow = new BrowserWindow({
-        title: 'NicoCommentMover',
-        left: 0,
-        top: 0,
-        width: size.width,
-        height: size.height,
-        transparent: true,
-        frame: false,
-        alwaysOnTop: true,
-        resizable: false,
-        hasShadow: false
-    });
-    mainWindow.setIgnoreMouseEvents(true);
-    mainWindow.maximize();
-    mainWindow.loadURL('file://' + __dirname + '/index.html');
-    mainWindow.on('closed', function() {
-        mainWindow = null;
-    });
-    // mainWindow.openDevTools();
+app.on('ready', () => {
+  const Screen = require('screen'),
+        size = Screen.getPrimaryDisplay().size;
+  mainWindow = new BrowserWindow({
+    title: 'NicoCommentMover',
+    left: 0,
+    top: 0,
+    width: size.width,
+    height: size.height,
+    transparent: true,
+    frame: false,
+    alwaysOnTop: true,
+    resizable: false,
+    hasShadow: false
+  });
+  // mainWindow.setIgnoreMouseEvents(true);
+  mainWindow.maximize();
+  mainWindow.loadURL(`file://${__dirname}/index.html`);
+  mainWindow.on('closed', () => mainWindow = null);
+  mainWindow.openDevTools();
 });
